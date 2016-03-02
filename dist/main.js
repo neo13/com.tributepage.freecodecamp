1db4b0d28cb07e9d9da18ef8e269ff2493af3f6d
Ahmad <a.derakhshan.iut@gmail.com>
2016-03-02 22:34:08 +0330

    mobile version ready

--- dist/main.js
+++ dist/main.js
@@ -51,6 +51,17 @@
 			    bottom: 20, 
 			}
 		});
+
+		$("button#offcanvas-nav-menu-btn").click(function (event) {
+			event.preventDefault();
+			event.stopPropagation();
+			$("div#side-nav").animate({left: '0px'}, 1500);
+
+			$(document).one('click', function (event) {
+				$("div#side-nav").animate({left: '-300px'}, 1500);			
+			});
+		});
+
 	});
 
 
